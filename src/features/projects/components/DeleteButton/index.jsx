e8dc71b5522c9e07e152acd4e3f159d9ca1af6b2
문형석 <hyn9xc@gmail.com>
2023-09-02 18:58:09 +0900

    feat: implement project deletion functionality

---
+++ src/features/projects/components/DeleteButton/index.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../../../../components/shared/Button";
+import Image from "../../../../components/shared/Image";
+
+import useDeleteProjectMutation from "../../hooks/useDeleteProjectMutation";
+
+import deleteIconUrl from "../../../../assets/delete_icon.svg";
+import Modal from "../../../../components/shared/Modal";
+
+function DeleteButton({ projectName, projectId }) {
+  const { mutate, isSuccess } = useDeleteProjectMutation();
+  const navigate = useNavigate("/");
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  function deleteProject(id) {
+    mutate({ projectId: id });
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/");
+    }
+  }, [ isSuccess, navigate ]);
+
+  return (
+    <>
+      <Button
+        onButtonClick={openModal}
+        appearance={{
+          display: "inline-block",
+          padding: "0",
+          margin: "0",
+          backgroundColor: "transparent",
+          boxShadow: "none",
+        }}
+      >
+        <Image
+          url={deleteIconUrl}
+          appearance={{ width: "20px", height: "20px" }}
+        />
+      </Button>
+      {isOpen && (
+        <Modal onModalClick={closeModal}>
+          <div css={{ padding: "20px" }}>
+            Are you sure you want to delete <strong>{projectName}</strong>?
+            <div css={{ display: "flex" }}>
+              <Button
+                onButtonClick={closeModal}
+                appearance={{
+                  marginRight: "5%",
+                  width: "95%",
+                  backgroundColor: "#fff",
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                onButtonClick={() => deleteProject(projectId)}
+                appearance={{
+                  width: "95%",
+                  backgroundColor: "#222",
+                  color: "#fff",
+                }}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default DeleteButton;
